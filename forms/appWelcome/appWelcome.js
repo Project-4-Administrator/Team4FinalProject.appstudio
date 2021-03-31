806e6eb3379b4dc6d25d75b3a8d9f13d6d306ae6
DBSunderland <davissunderland@creighton.edu>
2021-03-31 18:14:10 -0500

    3-31 18:14 Update

---
+++ forms/appWelcome/appWelcome.js
@@ -0,0 +1,22 @@
+appWelcome.onshow=function(){
+  lblWelcome.value = "[APP NAME] Login"
+  lblFooter.value = "Relationships have never been this easy."
+}
+
+btnLogin.onclick = function() {
+  ChangeForm(userLogin)
+}
+
+btnSignUp.onclick = function() {
+  ChangeForm(createAccount)
+}
+
+
+/*
+dessertVoting.onshow=function(){
+    drpDesserts.clear()   
+    for (i = 0; i < dessertList.length; i++) 
+        drpDesserts.addItem(dessertList[i])
+}
+*/
+
