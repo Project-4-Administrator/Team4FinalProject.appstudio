be8e179b4795ff696abcceaf11dab125b8dfb835
Henry Jiang <hjiang2000@gmail.com>
2021-04-05 14:10:37 -0500

    v2

--- calendarSimp.appstudio/forms/calSimp/calSimp.js
+++ calendarSimp.appstudio/forms/calSimp/calSimp.js
@@ -1,20 +1,39 @@
-let month= ["Jan", "Feb", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"]
-calSimp.onshow=function(){
- // always clear before populating
-    dpdMonth.clear()   
-    // put array of months in the dropdown (called populating it)
-    for (i = 0; i <month.length; i++) 
-        dpdMonth.addItem(month[i])
+let month = ["Jan", "Feb", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"]
+
+calSimp.onshow = function() {
+  // always clear before populating
+  dpdMonth.clear()
+  // put array of months in the dropdown (called populating it)
+  for (i = 0; i < month.length; i++)
+    dpdMonth.addItem(month[i])
 }
-s=""
-dpdMonth.onclick=function(){
-  if (typeof(s) == "object"){// means the control was clicked but no selection made yet
-    return                     // do nothing
+yourMonth = ""
+dpdMonth.onclick = function() {
+  if (typeof(yourMonth) == "object") { // means the control was clicked but no selection made yet
+    return // do nothing
   } else {
-    dpdMonth.value = s   // make dropdown show choice user made
-    console.log(`The user chose ${s} and .selection is ${dpdMonth.selection}.`)
+    dpdMonth.value = yourMonth // make dropdown show choice user made
+    console.log(`The user chose ${yourMonth} and .selection is ${dpdMonth.selection}.`)
   }
 }
 
 //date
-date=""
+date = ""
+
+inptDate.onchange = function() {
+  date = inptDate.value
+  console.log(`The date is ${date}`)
+}
+
+//activities
+activities = []
+numberAct = prompt("how many activities will you enter today?")
+
+for (i = 0; i < numberAct; i++) {
+  newActivity = prompt("Please enter your desired activity...")
+  activities.push(newActivity)
+  console.log(activities)
+}
+btnVote.onclick = function() {
+  ChangeForm(voteAct)
+}
