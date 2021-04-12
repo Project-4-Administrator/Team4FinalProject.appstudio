364b87984f4d7807adec8d2f7e02e91bf2d353bc
DBSunderland <davissunderland@creighton.edu>
2021-04-12 14:48:41 -0500

    4/12 14:48 update

--- forms/customizeAccount/customizeAccount.js
+++ forms/customizeAccount/customizeAccount.js
@@ -26,3 +26,21 @@ customizeAccount.onshow=function(){
   lblCustomize.value = 'Customize and view your profile!'
 }
 
+btnCustomerID.onclick=function(){
+  inputUsername = inptUsernameSearch.value
+  customer_id = query = `SELECT customer_id FROM customer WHERE username = "${inputUsername}"`
+    req = Ajax("https://ormond.creighton.edu/courses/375/ajax-connection.php", "POST", "host=ormond.creighton.edu&user=" + netID + "&pass=" + pw + "&database=" + database + "&query=" + query)
+
+    if (req.status == 200) {
+        results = JSON.parse(req.responseText)
+        if (results.length == 0)    
+           lblCustomize.value = `Your customer ID number is ${customer_id}`
+        else {        
+           lblCustomize.value = 'Something went wrong...'
+           
+        } 
+
+    }
+}
+
+
