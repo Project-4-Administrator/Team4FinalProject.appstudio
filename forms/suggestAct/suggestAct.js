5e4d2378c38e37b4e9800f588e6ed6f2689a1b8f
Henry Jiang <hjiang2000@gmail.com>
2021-04-14 14:31:41 -0500

    New optional feature

--- forms/suggestAct/suggestAct.js
+++ forms/suggestAct/suggestAct.js
@@ -1,13 +1,10 @@
-activities=[]
-
-
-
+let activities=[]
 
 btnSubmitAct.onclick=function(){
   activities.push(inptAct.value)
   console.log(activities)
 }
 
-btnSug.onclick=function(){
+btnSug.onclick=function(){ 
   ChangeForm(voteAct)
 }
--- forms/voteAct/voteAct.js
+++ forms/voteAct/voteAct.js
@@ -1,12 +1,50 @@
+selection = ""
+choice=[]
 
-dpdOptionsAct.onshow=function(){
-dpdOptionsAct.clear()
-for(i=0; i<activities.length; i++)
-  dpdOptionsAct.addItem(activities[i])
+dpdOptionsAct.onshow = function() {
+  dpdOptionsAct.clear()
+  for (i = 0; i < activities.length; i++)
+    dpdOptionsAct.addItem(activities[i])
 }
 
-btnVote.onclick=function(){
+dpdOptionsAct.onclick = function() {
+  if (typeof(selection) == "object") { // means the control was clicked 
+    return // but no selection made yet so do nothing
+  } else { // a selection made
+    dpdOptionsAct.value = selection // make dropdown show choice the user made
+    console.log(`The user chose ${selection}.`)
+  }
+}
+switch (activities.indexOf(selection)) {
+  case 0:
+    firstChoice + 1
+    console.log(firstChoice);
+  case 1:
+    secondChoice + 1
+    console.log(secondChoice);
+  case 2:
+    thirdChoice + 1;
+    (thirdChoice);
+  case 3:
+    fourthChoice + 1
+    (fourthChoice);
+  case 4:
+    fifthChoice + 1
+    (fifthChoice);;
+  default:
+   lblActResults.innerText = "Please make a proper selection"
+}
+function selectWin(firstChoice,secondChoice,thirdChoice,fourthChoice,fifthChoice) {
+  Math.max(firstChoice,secondChoice,thirdChoice,fourthChoice,fifthChoice)
+}
+
+btnVote.onclick = function() {
   ChangeForm(suggestAct)
 }
 
-
+/*
+the inputs updates the DB with insert
+1) get activities from the DB
+2)  populate the drop down with the data base
+3) vote "update" DB
+4) 
