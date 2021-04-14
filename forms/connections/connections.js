911a3e433c11943c33519020a2b496e4e236e60c
DBSunderland <davissunderland@creighton.edu>
2021-04-14 14:10:07 -0500

    4/14 14:10 Update

---
+++ forms/connections/connections.js
@@ -0,0 +1,27 @@
+hmbNavConnections.onclick = function(s) {
+  if (typeof(s) == "object") {
+    return
+  } else {
+    switch (s) {
+      case "Welcome":
+        ChangeForm(appWelcome)
+        break
+      case "Select":
+        ChangeForm(customerSelect)
+        break
+      case "Insert":
+        ChangeForm(customerInsert)
+        break
+      case "Create":
+        ChangeForm(createAccount)
+        break
+      case "Login":
+        ChangeForm(userLogin)
+        break
+    }
+  }
+}
+
+
+
+
