c9f6de1c32f88de87f9a7e35999daaf59dd2b02e
DBSunderland <davissunderland@creighton.edu>
2021-05-02 17:51:02 -0500

    Messages commit

--- forms/viewMessages/viewMessages.js
+++ forms/viewMessages/viewMessages.js
@@ -23,3 +23,21 @@ hmbNavSendMessages.onclick = function(s) {
 btnViewMessagesBack.onclick=function(){
   ChangeForm(sendMessages)
 }
+
+btnShowMessages.onclick=function(){
+  inputUsername = inptViewMessages.value
+  message = ''
+  query = `SELECT memo FROM message WHERE receive_user = "${inputUsername}"`
+    req = Ajax("https://ormond.creighton.edu/courses/375/ajax-connection.php", "POST", "host=ormond.creighton.edu&user=" + netID + "&pass=" + pw + "&database=" + database + "&query=" + query)
+
+    if (req.status == 200) {
+        results = JSON.parse(req.responseText)
+        if (results.length == 0)    
+           txtViewMessages.value = 'You have no messages.'
+        else {        
+           for (i = 0; i < results.length; i++)
+             message = message + results[i] + '\n'
+           txtViewMessages.value = message
+        } 
+    }
+}
